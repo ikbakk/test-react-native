e432d27214434beba8bf8df3cf4b105d82f803d0
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-08 00:56:27 +0800

    Home screen header and 'terjemahkan' button add

---
+++ src/Icons/Settings/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Icon from 'react-native-vector-icons/Feather'
+
+export default function SettingIcon({ size, color }) {
+  return (
+    <Icon
+      name='settings'
+      size={size}
+      color={color}
+    />
+  )
+}
--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import { View } from 'react-native'
-import { Text, Button } from '@rneui/themed'
+import { View, ScrollView } from 'react-native'
+import { Text, Button, Avatar } from '@rneui/themed'
+import SettingIcon from '../../Icons/Settings'
 
 function Home() {
   return (
-    <View>
-      <Text h1>Mari Belajar Muat an Lokal</Text>
-      <Button title="Solid Button"></Button>
-    </View>
+    <ScrollView style={{marginHorizontal:30}}>
+      <View
+        style={{
+          flexDirection:'row',
+          justifyContent: 'space-around',
+          alignItems:'center',
+          height:100,
+        }}
+      >
+        <Avatar size={40} rounded title='RJ' containerStyle={{ backgroundColor: 'purple',}} /> 
+        <Text style={{width:300,fontSize:18}}>Hai, Rafly Junizar!</Text>
+        <SettingIcon size={22} />
+      </View>
+      <Text style={{marginVertical:30, width:300, lineHeight:50, fontSize:40}} >Mari Belajar Muatan Lokal!</Text>
+      <View style={{marginBottom:30}} >
+        <Text style={{fontSize:22, paddingBottom:5}}>Coba Terjemahkan Kata</Text>
+        <Button 
+          title='Terjemahkan kata!'
+          buttonStyle={{backgroundColor:'#93D564', borderRadius:10, borderColor:'black', borderWidth:2, paddingVertical:15}}
+        />
+      </View>
+      <View>
+        <Text style={{fontSize:22, paddingBottom:10}}>Aksara</Text>
+      </View>
+    </ScrollView>
   )
 }
 export default Home
